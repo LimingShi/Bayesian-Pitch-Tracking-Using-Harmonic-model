7acdc2bcc4029fef5487d1316a97a577eb9e40ca
Liming Shi <limingshi12@foxmail.com>
2019-05-08 10:45:53 +0200

    update

--- run_colored_example.m
+++ run_colored_example.m
@@ -11,11 +11,11 @@ noise_s=factoryNoise;
 randstart=randi(length(noise_s)-length(cleanspeech)-100);
 noise_seg=noise_s(randstart:randstart+length(cleanspeech)-1);
 noise=addnoise_strict_snr(cleanspeech,noise_seg,SNR);
-speechSignal=cleanspeech+noise;
+NoisySignal=cleanspeech+noise;
 %% process the data 
 % the third argument is the pre-whitening flag,
 % when prew_flag=0,  pre-whitening will be disabled, and 
 % when prew_flag=1,  pre-whitening will be enabled
 plot_flag=1;
 prew_flag=1;
-result=BF0NLS(speechSignal,samplingFreq,plot_flag,prew_flag);
+result=BF0NLS(NoisySignal,samplingFreq,plot_flag,prew_flag);
--- run_white_example.m
+++ run_white_example.m
@@ -6,14 +6,14 @@ close all;
 %% generate noisy data
 SNR=0;
 noise=addnoise_strict_snr(cleanspeech,randn(size(cleanspeech)),SNR);
-speechSignal=cleanspeech+noise;
+NoisySignal=cleanspeech+noise;
 %% process the data 
 % the third argument is the pre-whitening flag,
 % when prew_flag=0,  pre-whitening will be disabled, and 
 % when prew_flag=1,  pre-whitening will be enabled
 plot_flag=1;
 prew_flag=0;
-result=BF0NLS(speechSignal,samplingFreq,plot_flag,prew_flag);
+result=BF0NLS(NoisySignal,samplingFreq,plot_flag,prew_flag);
 
 
 
