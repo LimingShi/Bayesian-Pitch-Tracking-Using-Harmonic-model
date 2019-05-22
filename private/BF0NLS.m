71c79a12a75d34ff25f5ae47d3209ae5fd628928
Liming Shi <limingshi12@foxmail.com>
2019-05-22 18:16:30 +0200

    update

--- private/BF0NLS.m
+++ private/BF0NLS.m
@@ -1,49 +1,93 @@
-function result=BF0NLS(speechSignal,samplingFreq,plot_flag,prew_flag)
+function result=BF0NLS(speechSignal,samplingFreq,plot_flag,prew_flag,Par_set)
+
+% -----------------------------------------------------------------------
 % format of the input and output arguments
+% 
 % input:
 % input arguments --> as the names suggested
+% Par_set.segmentTime is the segmentTime in seconds
+% Par_set.segmentShift is the segmentShift in seconds
+% Par_set.f0Bounds is the f0 bounds in Hz
+% 
 % output:
-% result.tt         -->     time vector
-% result.ff         -->     Fundamental frequency estimates (when all the frames are considered as voiced)
-% result.oo         -->     order estimates  (when all the frames are considered as voiced)
-% result.vv         -->     voicing probability
-% result.best_ff    -->     Best fundamental frequency estimates (setting the F0=nan when voicing probability is less than .5)
-% result.best_order -->     Best harmonic order estimates (setting the order=nan when voicing probability is less than .5)
-
+% result.tt         -->   time vector
+% result.ff         -->   Fundamental frequency estimates
+%                         (when all the frames are considered as voiced)
+% result.oo         -->   order estimates  
+%                         (when all the frames are considered as voiced)
+% result.vv         -->   voicing probability
+% result.best_ff    -->   Best fundamental frequency estimates (setting the F0=nan 
+%                         when voicing probability is less than .5)
+% result.best_order -->   Best harmonic order estimates (setting the order=nan 
+%                         when voicing probability is less than .5)
+% 
+% Example for for customize Par_set
+% 
+% Par_set.segmentTime = 0.03; 30 ms for each segment
+% Par_set.segmentShift = 0.01; 10 ms for segment shift
+% Par_set.f0Bounds =[70, 400]; pitch is bounded between 70 to 400 Hz
+% 
+% 
 % Written by Liming Shi, Aalborg 9000, Denmark
 % Email: ls@create.aau.dk
+% -----------------------------------------------------------------------
 
 
 if nargin<3
 %   do not use prewhitening in default, and do not plot
     plot_flag=0;
     prew_flag=0; 
+    Par_set=[];
 end
 if nargin<4
 %   do not use prewhitening in default
     prew_flag=0; 
+    Par_set=[];
+end
+if nargin<5
+%   use default parameter set
+    Par_set=[];
 end
 
 %% resample to 16 KHz for the best results
 fs_fine_tuned=16000;
 speechSignal=resample(speechSignal,fs_fine_tuned,samplingFreq);
 samplingFreq=fs_fine_tuned;
+if isempty(Par_set)
+    segmentTime = 0.025; %     seconds
+    segmentShift = 0.010; % seconds
+    f0Bounds = [70, 400]; % cycles/sample
+else
+    if isfield('segmentTime')
+        segmentTime = Par_set.segmentTime; %     seconds
+    else
+        segmentTime = 0.025; %     seconds
+    end
+    if isfield('segmentTime')
+        segmentShift = Par_set.segmentShift; 
+    else
+        segmentShift = 0.010; % seconds
+    end
+    if isfield('segmentTime')
+        f0Bounds = Par_set.f0Bounds/samplingFreq; % cycles/sample
+    else
+        f0Bounds = [70, 400]; % cycles/sample
+    end
+end
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % normalization step
 sumE = sqrt(speechSignal'*speechSignal/length(speechSignal));
 scale = sqrt(3.1623e-5)/sumE; % scale to -45-dB loudness level
 speechSignal=speechSignal*scale;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 nData = length(speechSignal);
-
 % set up
-segmentTime = 0.025; %     seconds
+f0Bounds=f0Bounds/samplingFreq;
 segmentLength = round(segmentTime*samplingFreq/2)*2; % samples
-segmentShift = 0.010; % seconds
 nShift = round(segmentShift*samplingFreq); % samples
 nSegments = floor((nData+segmentLength/2-segmentLength)/nShift)+1;
-f0Bounds = [70, 400]/samplingFreq; % cycles/sample
+
 if prew_flag==0
     maxNoHarmonics = 10;
 else
