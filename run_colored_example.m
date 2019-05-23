e6e0e0dd3453563b9d8bfcd3681d0613caa0fcce
Liming Shi <limingshi12@foxmail.com>
2019-05-23 10:49:25 +0200

    update

--- run_colored_example.m
+++ run_colored_example.m
@@ -12,9 +12,15 @@ noise_seg=noise(randstart:randstart+length(cleanspeech)-1);
 scaled_noise=addnoise(cleanspeech,noise_seg,SNR);
 NoisySignal=cleanspeech+scaled_noise;
 %% process the data 
-% the third argument is the pre-whitening flag,
+% the third input argument is the plot flag,
+% when plot_flag=0,  results will not be given in figure;
+% when plot_flag=1,  results will be given in figure;
+
+% the fourth input argument is the pre-whitening flag,
 % when prew_flag=0,  pre-whitening will be disabled, and 
 % when prew_flag=1,  pre-whitening will be enabled
+
+% the fifth input argument is the parameter set you can customize yourself,
 plot_flag=1;
 prew_flag=1;
 tic
--- run_music_example.m
+++ run_music_example.m
@@ -1,7 +1,7 @@
 clear
 clc
 close all;
-%% read speech signals
+%% read music signals
 [vib_flute, samplingFreq] = audioread(['vib_flute.wav']);vib_flute=vib_flute(:,1);
 %% generate noisy data
 SNR=0;
@@ -9,9 +9,15 @@ noise=randn(size(vib_flute)); % White Gaussian noise
 scaled_noise=addnoise(vib_flute,noise,SNR);
 NoisySignal=vib_flute+scaled_noise;
 %% process the data 
-% the third argument is the pre-whitening flag,
+% the third input argument is the plot flag,
+% when plot_flag=0,  results will not be given in figure;
+% when plot_flag=1,  results will be given in figure;
+
+% the fourth input argument is the pre-whitening flag,
 % when prew_flag=0,  pre-whitening will be disabled, and 
 % when prew_flag=1,  pre-whitening will be enabled
+
+% the fifth input argument is the parameter set you can customize yourself,
 plot_flag=1;
 prew_flag=0;
 Par_set.f0Bounds = [100, 1000]; % Pitch boundaries for music signal (prior information)
